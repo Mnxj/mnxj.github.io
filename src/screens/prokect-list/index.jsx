ba04459ea36011b3126674cbe80b9a7d3c394918
tianhang <tianhang.shi@thoughtworks.com>
2021-12-11 22:19:13 +0800

    feat: add useMount and useDebounce

--- src/screens/prokect-list/index.jsx
+++ src/screens/prokect-list/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {SearchPanel} from "./search-panel";
 import {useEffect, useState} from "react";
 import {List} from "./list";
-import {cleanObject} from "../../utils";
+import {cleanObject, useDebounce, useMount} from "../../utils";
 import * as qs from "qs";
 
 const apiUrl=process.env.REACT_APP_API_URL
@@ -14,22 +14,22 @@ export const ProjectListScreen = () =>{
         personId:''
     })
     const [list,setList] = useState([])
-
+    const debounce  = useDebounce(param,2000)
     useEffect(()=>{
-        fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(param))}`).then(async response => {
+        fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(debounce))}`).then(async response => {
             if (response.ok){
                 setList(await response.json())
             }
         })
-    },[param])
+    },[debounce])
 
-    useEffect(()=>{
+    useMount(()=>{
         fetch(`${apiUrl}/users`).then(async response => {
             if (response.ok){
                 setUsers(await response.json())
             }
         })
-    },[param])
+    })
     return <div>
         <SearchPanel users={users} param={param} setParam={setParam}/>
         <List users={users} list={list}/>
