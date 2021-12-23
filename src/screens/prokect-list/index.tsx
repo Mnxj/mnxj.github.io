54b6c57f28e7b2ed383efa28e7ef0cfb93672e9f
tianhang <tianhang.shi@thoughtworks.com>
2021-12-23 21:05:59 +0800

    feat:add login

--- src/screens/prokect-list/index.tsx
+++ src/screens/prokect-list/index.tsx
@@ -2,10 +2,7 @@ import { SearchPanel } from "./search-panel";
 import { useEffect, useState } from "react";
 import { List } from "./list";
 import { cleanObject, useDebounce, useMount } from "../../utils";
-import * as qs from "qs";
-
-const apiUrl = process.env.REACT_APP_API_URL;
-
+import { useHttp } from "../../utils/http";
 export const ProjectListScreen = () => {
   const [users, setUsers] = useState([]);
 
@@ -15,22 +12,13 @@ export const ProjectListScreen = () => {
   });
   const [list, setList] = useState([]);
   const debounce = useDebounce(param, 200);
+  const client = useHttp();
   useEffect(() => {
-    fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(debounce))}`).then(
-      async (response) => {
-        if (response.ok) {
-          setList(await response.json());
-        }
-      }
-    );
+    client("projects", { data: cleanObject(debounce) }).then(setList);
   }, [debounce]);
 
   useMount(() => {
-    fetch(`${apiUrl}/users`).then(async (response) => {
-      if (response.ok) {
-        setUsers(await response.json());
-      }
-    });
+    client("users").then(setUsers);
   });
 
   return (
