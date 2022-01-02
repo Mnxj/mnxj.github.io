686bcbcfd6b93907086f9e1ba8fe5a670432368a
tianhang <tianhang.shi@thoughtworks.com>
2022-01-02 23:03:10 +0800

    feat:用use-select.tsx选择用户

--- src/screens/project-list/index.tsx
+++ src/screens/project-list/index.tsx
@@ -1,24 +1,31 @@
 import { SearchPanel } from "./search-panel";
-import { useState } from "react";
 import { List } from "./list";
 import { useDebounce, useDocumentTitle } from "../../utils";
 import styled from "@emotion/styled";
 import { Typography } from "antd";
 import { useProject } from "../../utils/project";
 import { useUsers } from "../../utils/user";
-import { useUrlQueryParam } from "../../utils/url";
+import { useProjectSearchParams } from "./util";
 
 export const ProjectListScreen = () => {
-  const [keys] = useState<("name" | "personId")[]>(["name", "personId"]);
-  const [param, setParam] = useUrlQueryParam(keys);
-  const debounce = useDebounce(param, 200);
-  const { isLoading, error, data: list } = useProject(debounce);
-  const { data: users } = useUsers();
   useDocumentTitle("项目列表", false);
+
+  const [param, setParam] = useProjectSearchParams();
+  const { isLoading, error, data: list } = useProject(useDebounce(param, 200));
+  const { data: users } = useUsers();
 
   return (
     <Container>
       <h1>项目列表</h1>
+      <select
+        onChange={(evt) => {
+          const value = evt.target.value;
+          console.log(value, typeof value);
+        }}
+      >
+        <option value={undefined}>默认属性</option>
+        <option value={1}>第一个选项</option>
+      </select>
       <SearchPanel users={users || []} param={param} setParam={setParam} />
       {error ? (
         <Typography.Text type={"danger"}>{error.message}</Typography.Text>
