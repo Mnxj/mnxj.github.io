afa86cc8d3b340594e95123146ea201b46787ff7
tianhang <tianhang.shi@thoughtworks.com>
2022-01-04 14:44:15 +0800

    feat:项目收藏功能

--- src/screens/project-list/index.tsx
+++ src/screens/project-list/index.tsx
@@ -11,26 +11,27 @@ export const ProjectListScreen = () => {
   useDocumentTitle("项目列表", false);
 
   const [param, setParam] = useProjectSearchParams();
-  const { isLoading, error, data: list } = useProject(useDebounce(param, 200));
+  const {
+    isLoading,
+    error,
+    data: list,
+    retry,
+  } = useProject(useDebounce(param, 200));
   const { data: users } = useUsers();
 
   return (
     <Container>
       <h1>项目列表</h1>
-      <select
-        onChange={(evt) => {
-          const value = evt.target.value;
-          console.log(value, typeof value);
-        }}
-      >
-        <option value={undefined}>默认属性</option>
-        <option value={1}>第一个选项</option>
-      </select>
       <SearchPanel users={users || []} param={param} setParam={setParam} />
       {error ? (
         <Typography.Text type={"danger"}>{error.message}</Typography.Text>
       ) : null}
-      <List loading={isLoading} users={users || []} dataSource={list || []} />
+      <List
+        refresh={retry}
+        loading={isLoading}
+        users={users || []}
+        dataSource={list || []}
+      />
     </Container>
   );
 };
--- src/utils/use-async.ts
+++ src/utils/use-async.ts
@@ -25,6 +25,9 @@ export const useAsync = <D>(
     ...defaultInitialState,
     ...initialState,
   });
+  // 惰性初始化
+  const [retry, setRetry] = useState(() => () => {});
+
   const setData = (data: D) =>
     setState({
       data,
@@ -39,10 +42,18 @@ export const useAsync = <D>(
       data: null,
     });
   //用来出发异步请求
-  const run = (promise: Promise<D>) => {
+  const run = (
+    promise: Promise<D>,
+    runConfig?: { retry: () => Promise<D> }
+  ) => {
     if (!promise || !promise.then()) {
       throw new Error("请传入 Promise 类型数据");
     }
+    setRetry(() => () => {
+      if (runConfig?.retry) {
+        run(runConfig?.retry(), runConfig);
+      }
+    });
     setState({ ...state, stat: "loading" });
     return promise
       .then((data) => {
@@ -63,6 +74,7 @@ export const useAsync = <D>(
     run,
     setData,
     setError,
+    retry,
     ...state,
   };
 };
